376c130b28083315a279ef6254bfa2cfff3ddd10
Emerson Duarte <raasjava@gmail.com>
2021-03-14 22:11:38 -0300

    Backend: Ajustes

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -2,9 +2,9 @@ var conn = require('../database/connection');
 
 module.exports = {
     async index(req, res){
-        var dadosForm = req.body;
-        console.log(dadosForm)
-        conn.raw(`SELECT name FROM employee WHERE email='${dadosForm.email}';`).then((result) => {
+        var email = req.params.email;
+        console.log(email)
+        conn.raw(`SELECT name FROM employee WHERE email='${email}';`).then((result) => {
           if(result.length>0){
             res.status(200).send({sucesso: 'Email encontrado'});
             res.end();
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,141 +1,36 @@
-import { useEffect, useState } from 'react'
-import {Avatar, Box, Button, Checkbox, CssBaseline, FormControlLabel, Link, makeStyles, Paper, TextField, Typography} from '@material-ui/core'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Grid from '@material-ui/core/Grid';
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import {Box, Button, Container, TextField} from '@material-ui/core'
 
 import api from "../services/api";
 
+
+interface LoginParam {
+  email: string;
+}
+
 export default function Login(){
+  
+  const params = useParams<LoginParam>();
 
-    function Copyright() {
-        return (
-          <Typography variant="body2" color="textSecondary" align="center">
-            {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
-              Your Website
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-          </Typography>
-        );
-      }
-      
-    const useStyles = makeStyles((theme) => ({
-        root: {
-          height: '100vh',
-        },
-        image: {
-          backgroundImage: 'url(https://source.unsplash.com/random)',
-          backgroundRepeat: 'no-repeat',
-          backgroundColor:
-            theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        },
-        paper: {
-          margin: theme.spacing(8, 4),
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        },
-        avatar: {
-          margin: theme.spacing(1),
-          backgroundColor: theme.palette.secondary.main,
-        },
-        form: {
-          width: '100%', // Fix IE 11 issue.
-          marginTop: theme.spacing(1),
-        },
-        submit: {
-          margin: theme.spacing(3, 0, 2),
-        },
-    }));
-    
-    const classes = useStyles();
+  useEffect(() => {
+    api.get('authenticate').then(data => {
+      console.log(params)
+    })
+},);
 
-    interface Log {
-        email: string;
-    }
-    const [login, setLogin] = useState<Log>();
-    
-    useEffect(() => {
-        api.get('/authenticate').then(data => {
-            setLogin(data.data);
-            console.log(data)
-        })
-    },)
-    if(login){
-        return <p>Carregando...</p>;
-    }
-    
-    return (
-      <Grid container component="main" className={classes.root}>
-      <CssBaseline />
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Login
-          </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            
-            {/* <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            /> */}
-            
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Lembrar"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Entrar
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Esqueceu sua senha?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Não tem conta? Clique aqui"}
-                </Link>
-              </Grid>
-            </Grid>
-            <Box mt={5}>
-              <Copyright />
-            </Box>
+  return(
+   
+    <Container maxWidth="sm">
+         <Box component="span" m={1}>
+          <form  noValidate autoComplete="off">
+              <TextField id="outlined-basic" name="email" label="E-mail" variant="outlined" />
           </form>
-        </div>
-      </Grid>
-    </Grid>
-  );
+          <br></br>
+          <Button  type="submit" variant="contained" color="primary">Entrar</Button>
+        </Box>
+       
+    </Container>
+    
+    )
 }
